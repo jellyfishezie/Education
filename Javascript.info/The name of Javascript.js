235cf98560e427ae91b835a8b9b194f0c6e51682
Jellyfishezie <Jellyfishezie@gmail.com>
2019-01-28 22:10:20 +0100

    HTML Lesson Files Added

---
+++ Javascript.info/The name of Javascript.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+	<script>
+	'use strict';
+
+	let nameJavascript = prompt('What is the "official" name of Javascript)
+	
+	if (nameJavascript == "ECMAScript") {
+		alert('That Is Right!')
+	} else {
+		alert("That Is Wrong!")
+	}
+	
+	</script>
+
+</body>
+</html>
